b321f93ac093b8fe9d604c14ded1080b7e4dcc5f
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-07-04 13:21:08 +0200

    animation data compression

--- Assets/CSharp/AnimationExporter.cs
+++ Assets/CSharp/AnimationExporter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 
 public class AnimationExporter : MonoBehaviour
@@ -16,52 +17,84 @@ public class AnimationExporter : MonoBehaviour
 		return Enumerable.Range(0, transform.childCount).Select(transform.GetChild).SelectMany(t => new Transform[] { t }.Concat(Descendants(t)));
 	}
 
-	struct Bone
+	class Bone
 	{
 		public readonly Matrix4x4 matrix;
 		public readonly string name;
+		public readonly float time;
 
-        public Bone(Matrix4x4 matrix, string name)
+        public Bone(Matrix4x4 matrix, string name, float time)
         {
             this.matrix = matrix;
             this.name = name;
+			this.time = time;
         }
     }
 
-	struct Line
+	class Point
+	{
+		public readonly float time;
+		public readonly Matrix4x4 matrix;
+
+        public Point(float time, Matrix4x4 matrix)
+        {
+            this.time = time;
+            this.matrix = matrix;
+        }
+    }
+
+	class Line
 	{
 		public readonly string name;
-		public readonly Matrix4x4[] matrices;
+		public readonly Point[] points;
 
-        public Line(string name, Matrix4x4[] matrices)
+        public Line(string name, Point[] points)
         {
             this.name = name;
-            this.matrices = matrices;
+            this.points = points;
         }
     }
 
-	static Bone TransformFrame(Matrix4x4 w2l, Transform transform)
+	class PointComparer : IEqualityComparer<Point>
 	{
-		return new Bone(w2l * transform.localToWorldMatrix, transform.name);
+        public bool Equals(Point x, Point y)
+        {
+			return x.matrix == y.matrix;
+        }
+
+        public int GetHashCode(Point obj)
+        {
+			return obj.matrix.GetHashCode();
+        }
+    }
+
+	static Line CompressLine(Line line)
+	{
+		var points = line.points.Distinct(new PointComparer()).ToArray();
+		return new Line(line.name, points);
+	}
+
+	static Bone TransformFrame(Matrix4x4 w2l, Transform transform, float time)
+	{
+		return new Bone(w2l * transform.localToWorldMatrix, transform.name, time);
 	}
 	static Bone[] AnimationFrame(GameObject root, AnimationClip clip, float time, Matrix4x4 w2l, Transform[] bones)
 	{
-		clip.SampleAnimation(root, time);
-		return bones.Select(b => TransformFrame(w2l, b)).ToArray();
+		return bones.Select(b => TransformFrame(w2l, b, time)).ToArray();
 	}
 	static Line[] AnimationLines(List<Bone[]> frames)
 	{
 		if (frames.Count == 0) return new Line[0];
-		var map = new Dictionary<string, List<Matrix4x4>>();
+		var map = new Dictionary<string, List<Point>>();
 		foreach (var bone in frames.SelectMany(f => f))
 		{
-			List<Matrix4x4> list;
+			List<Point> list;
 			if (!map.TryGetValue(bone.name, out list))
 			{
-				list = new List<Matrix4x4>();
+				list = new List<Point>();
 				map[bone.name] = list;
 			}
-			list.Add(bone.matrix);
+			list.Add(new Point(bone.time, bone.matrix));
 		}
 		return map.Select(p => new Line(p.Key, p.Value.ToArray())).ToArray();
 	}
@@ -69,11 +102,33 @@ public class AnimationExporter : MonoBehaviour
 	[ContextMenu("Create animation")]
 	void CreateAnimation ()
 	{
+		if (!EditorApplication.isPlaying)
+		{
+			Debug.Log("Enter play mode first");
+			return;
+		}
+		
+		StartCoroutine(CreateAnimation(gameObject, animationClip, outputPath));
+	}
+	static IEnumerator CreateAnimation(GameObject gameObject, AnimationClip animationClip, string outputPath)
+	{
 		var w2l = gameObject.transform.worldToLocalMatrix;
 		var bones = Descendants(gameObject.transform).ToArray();
 		var frameCount = (int)(animationClip.length * animationClip.frameRate);
-        var frames = Enumerable.Range(0, frameCount).Select(f => AnimationFrame(gameObject, animationClip, f / frameCount, w2l, bones)).ToList();
-		var lines = AnimationLines(frames);
+		List<Bone[]> frames = new List<Bone[]>();
+
+		foreach (var f in Enumerable.Range(0, frameCount))
+		{
+			var t = f / (float)frameCount;
+			animationClip.SampleAnimation(gameObject, t);
+			yield return new WaitForEndOfFrame();
+			frames.Add(AnimationFrame(gameObject, animationClip, t, w2l, bones));
+		}
+
+		var lines = AnimationLines(frames).Select(CompressLine).ToArray();
+
+		yield return new WaitForEndOfFrame();
+
 		var writer = new StringBuilder();
 		WriteAnimation(s => writer.Append(s), animationClip.name, animationClip.length, frameCount, lines);
 		Directory.CreateDirectory(outputPath);
@@ -98,7 +153,12 @@ public class AnimationExporter : MonoBehaviour
 		writer("\n\t{");
 		writer(string.Format(":name \"{0}\"", line.name));
 		writer("\n\t\t[");
-		foreach (var m in line.matrices.Select(MatrixToArray).Select(FloatToStringArray)) writer(string.Format("\n\t\t\t[{0}]", string.Join(" ", m)));
+		foreach (var l in line.points) writer(
+			string.Format("\n\t\t\t[{0} [{1}]]",
+				l.time,
+				string.Join(" ", FloatToStringArray(MatrixToArray(l.matrix)))
+			)
+		);
 		writer("\n\t\t]");
 		writer("\n\t}");
 	}
@@ -107,12 +167,5 @@ public class AnimationExporter : MonoBehaviour
 		writer("{ " + string.Format(":name \"{0}\" :length {1} :frames {2} :bones [ ", name, length, frameCount));
 		foreach (var line in lines) WriteLine(writer, line);
 		writer("\n] }");
-	}
-
-	void Update ()
-	{
-		var t = Time.time;
-		if (animationClip.isLooping) t = Mathf.Repeat(t, animationClip.length);
-        animationClip.SampleAnimation(gameObject, t);
 	}
 }
