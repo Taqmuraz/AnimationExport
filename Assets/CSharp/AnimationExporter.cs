81c876516d4219752f1e2ae2d4cf79ad7d23523d
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2024-08-01 13:20:43 +0200

    compressed matrices

--- Assets/CSharp/AnimationExporter.cs
+++ Assets/CSharp/AnimationExporter.cs
@@ -18,19 +18,31 @@ public class AnimationExporter : MonoBehaviour
 	struct TransformState
 	{
 		public Vector3 position;
-		public Quaternion rotation;
+		public Vector3 rotation;
 		public Vector3 scale;
 
 		public static TransformState Create(Transform transform)
 		{
 			var t = new TransformState();
 			t.position = transform.localPosition;
-			t.rotation = transform.localRotation;
+			t.rotation = transform.localEulerAngles;
 			t.scale = transform.localScale;
 			return t;
 		}
 
-		public Matrix4x4 LocalMatrix() { return Matrix4x4.TRS(position, rotation, scale); }
+		public bool Equals(TransformState t)
+		{
+			return position == t.position && rotation == t.rotation && scale == t.scale;
+		}
+
+        public override int GetHashCode()
+        {
+			int hash = 17;
+			hash = hash * 23 + position.GetHashCode();
+			hash = hash * 23 + rotation.GetHashCode();
+			hash = hash * 23 + scale.GetHashCode();
+			return hash;
+		}
     }
 
 	Dictionary<Transform, TransformState> states = new Dictionary<Transform, TransformState>();
@@ -47,7 +59,7 @@ public class AnimationExporter : MonoBehaviour
 		{
 			var t = states[d];
 			d.localPosition = t.position;
-			d.localRotation = t.rotation;
+			d.localEulerAngles = t.rotation;
 		}
 	}
 
@@ -58,13 +70,13 @@ public class AnimationExporter : MonoBehaviour
 
 	class Bone
 	{
-		public readonly Matrix4x4 matrix;
+		public readonly TransformState transform;
 		public readonly string name;
 		public readonly float time;
 
-        public Bone(Matrix4x4 matrix, string name, float time)
+        public Bone(TransformState transform, string name, float time)
         {
-            this.matrix = matrix;
+            this.transform = transform;
             this.name = name;
 			this.time = time;
         }
@@ -73,12 +85,12 @@ public class AnimationExporter : MonoBehaviour
 	class Point
 	{
 		public readonly float time;
-		public readonly Matrix4x4 matrix;
+		public readonly TransformState transform;
 
-        public Point(float time, Matrix4x4 matrix)
+        public Point(float time, TransformState transform)
         {
             this.time = time;
-            this.matrix = matrix;
+            this.transform = transform;
         }
     }
 
@@ -98,12 +110,12 @@ public class AnimationExporter : MonoBehaviour
 	{
         public bool Equals(Point x, Point y)
         {
-			return x.matrix == y.matrix;
+			return x.transform.Equals(y.transform);
         }
 
         public int GetHashCode(Point obj)
         {
-			return obj.matrix.GetHashCode();
+			return obj.transform.GetHashCode();
         }
     }
 
@@ -114,17 +126,17 @@ public class AnimationExporter : MonoBehaviour
 		else return line;
 	}
 
-	Bone TransformFrame(Func<Transform, Matrix4x4> source, Transform transform, float time)
+	Bone TransformFrame(Transform transform, float time)
 	{
 		var t = TransformState.Create(transform);
 		t.position.x *= scaleMultiplier.x;
 		t.position.y *= scaleMultiplier.y;
 		t.position.z *= scaleMultiplier.z;
-		return new Bone(t.LocalMatrix(), transform.name, time);
+		return new Bone(t, transform.name, time);
 	}
-	Bone[] AnimationFrame(float time, Func<Transform, Matrix4x4> w2l, Transform[] bones)
+	Bone[] AnimationFrame(float time, Transform[] bones)
 	{
-		return bones.Select(b => TransformFrame(w2l, b, time)).ToArray();
+		return bones.Select(b => TransformFrame(b, time)).ToArray();
 	}
 	static Line[] AnimationLines(List<Bone[]> frames)
 	{
@@ -138,7 +150,7 @@ public class AnimationExporter : MonoBehaviour
 				list = new List<Point>();
 				map[bone.name] = list;
 			}
-			list.Add(new Point(bone.time, bone.matrix));
+			list.Add(new Point(bone.time, bone.transform));
 		}
 		return map.Select(p => new Line(p.Key, p.Value.ToArray())).ToArray();
 	}
@@ -187,13 +199,15 @@ public class AnimationExporter : MonoBehaviour
 	{
 		foreach (var animationClip in animationClips)
 		{
-			yield return StartCoroutine(CreateAnimation(gameObject, t => states[t].LocalMatrix(), animationClip));
+			yield return StartCoroutine(CreateAnimation(gameObject, animationClip));
 			ResetStates();
 			yield return new WaitForEndOfFrame();
 		}
 	}
 
-	IEnumerator CreateAnimation(GameObject gameObject, Func<Transform, Matrix4x4> w2l, AnimationClip animationClip)
+	static List<string> vectors = new List<string>();
+
+	IEnumerator CreateAnimation(GameObject gameObject, AnimationClip animationClip)
 	{
 		var bones = Descendants(gameObject.transform).Where(t => t.gameObject.tag != "Ignore").ToArray();
 		var framePreprocessors = GetComponentsInChildren<IFramePreprocessor>();
@@ -212,7 +226,7 @@ public class AnimationExporter : MonoBehaviour
 			animationClip.SampleAnimation(gameObject, t);
 			yield return new WaitForEndOfFrame();
 			proc();
-			frames.Add(AnimationFrame(t, w2l, bones));
+			frames.Add(AnimationFrame(t, bones));
 		}
 
 		var lines = AnimationLines(frames).Select(CompressLine).ToArray();
@@ -220,7 +234,9 @@ public class AnimationExporter : MonoBehaviour
 		yield return new WaitForEndOfFrame();
 
 		var writer = new StringBuilder();
+		vectors.Clear();
 		WriteAnimation(s => writer.Append(s), animationClip.name, animationClip.length, lines);
+		Debug.Log(string.Format("{0}/{1}", vectors.Count, vectors.Distinct().Count()));
 		foreach (var outputPath in outputPaths)
 		{
 			var dir = Path.Combine(outputPath, directoryName);
@@ -232,33 +248,61 @@ public class AnimationExporter : MonoBehaviour
 	}
 
 	delegate void Writer(string text);
+	delegate int ResourceWriter(string text);
 
-	static float[] MatrixToArray(Matrix4x4 m)
+	static void WriteArray<T>(Writer writer, ResourceWriter resourceWriter, T[] array, Func<T, string> toString)
 	{
-		return Enumerable.Range(0, 16).Select(i => m[i]).ToArray();
-	}
-	static string[] FloatToStringArray(float[] f)
-	{
-		return f.Select(v => v.ToString("F3")).ToArray();
+		int i = resourceWriter(string.Format("[{0}]", (string.Join(" ", array.Select(toString).ToArray()))));
+		writer(i.ToString() + " ");
 	}
 
-	static void WriteLine(Writer writer, Line line)
+	static void WriteVector3(Writer writer, ResourceWriter resourceWriter, Vector3 vec)
+	{
+		WriteArray(writer, resourceWriter, new float[] { vec.x, vec.y, vec.z }, f => f.ToString("F3"));
+	}
+
+	static void WriteTransform(Writer writer, ResourceWriter resourceWriter, TransformState transform)
+	{
+		WriteVector3(writer, resourceWriter, transform.position);
+		WriteVector3(writer, resourceWriter, transform.rotation);
+		WriteVector3(writer, resourceWriter, transform.scale);
+	}
+
+	static void WriteLine(Writer writer, ResourceWriter resourceWriter, Line line)
 	{
 		writer("\n\t");
 		writer(string.Format("\"{0}\"", line.name));
 		writer(" [");
-		foreach (var l in line.points) writer(
-			string.Format("\n\t\t[{0} [{1}]]",
-				l.time,
-				string.Join(" ", FloatToStringArray(MatrixToArray(l.matrix)))
-			)
-		);
+		foreach (var l in line.points)
+		{
+			writer(string.Format("\n\t\t[ {0} [ ", l.time));
+			WriteTransform(writer, resourceWriter, l.transform);
+			writer("] ]");
+		}
 		writer("\n\t]");
 	}
 	static void WriteAnimation(Writer writer, string name, float length, Line[] lines)
 	{
+		Dictionary<string, int> keys = new Dictionary<string, int>();
+		List<string> values = new List<string>();
+		ResourceWriter resourceWriter = r =>
+		{
+			int i;
+			if (keys.TryGetValue(r, out i))
+			{
+				return i;
+			}
+			else
+			{
+				int c = values.Count;
+				values.Add(r);
+				return keys[r] = c;
+			}
+		};
+
 		writer("{ " + string.Format(":name \"{0}\" :length {1} :bones", name, length) + " { ");
-		foreach (var line in lines) WriteLine(writer, line);
+		foreach (var line in lines) WriteLine(writer, resourceWriter, line);
+		writer(string.Format("\n\t:resources [\n\t\t{0}]", string.Join("\n\t\t", values.ToArray())));
 		writer("\n} }");
 	}
 }
---
+++ Assets/CSharp/Clone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clone : MonoBehaviour
+{
+	[SerializeField] GameObject target;
+	[SerializeField] int times;
+
+	void Start ()
+	{
+		for(int i = 0; i < times; i++)
+		{
+			int x = i % 10;
+			int y = 0;
+			int z = i / 10;
+			var obj = GameObject.Instantiate(target);
+			obj.transform.position = new Vector3(x, y, z) * 2;
+			obj.GetComponent<Animator>().CrossFade("Attack", i * 0.1f);
+		}
+	}
+}
