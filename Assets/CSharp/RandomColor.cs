c7b00cc45eba27180c2a029bc9e3f9da5c1f11f3
lulook <lulookgamedev@gmail.com>
2024-11-25 17:01:04 +0100

    walk-crowd-anims

---
+++ Assets/CSharp/RandomColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+	[SerializeField] Renderer target;
+
+	// Use this for initialization
+	void Start () {
+		foreach (var mat in target.materials) mat.color = new Color(Random.value, Random.value, Random.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/CSharp/RandomWalker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWalker : MonoBehaviour {
+
+	[SerializeField] Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		body.velocity = transform.forward * 6;
+		transform.eulerAngles += Vector3.up * Time.deltaTime * 45;
+	}
+}
